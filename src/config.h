c56b0c8faa3af70a2bf98244a0fbded6a272e34f
Alberto G. Jácome <agjacome@gmail.com>
2017-08-13 14:30:34 +0200

    apply viewontag patch

--- src/config.h
+++ src/config.h
@@ -26,6 +26,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const Bool viewontag         = True;     /* Switch view on tag switch */
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
